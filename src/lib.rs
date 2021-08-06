9319c5b09c7a384f43c0d00dffd13aaff4539056
Maxwell Flitton <maxwellflitton@maxwells-mbp.mynet>
2021-08-07 00:11:43 +0100

    basics working

--- src/lib.rs
+++ src/lib.rs
@@ -31,15 +31,15 @@ enum Value {
 
 
 #[pyfunction]
-fn get_model<'a>(event_ids: Vec<i32>, mut base_path: String) -> Vec<HashMap<String, Value>> {
+fn get_model<'a>(event_ids: Vec<i32>, mut base_path: String, py: Python) -> Vec<PyDict> {
     let footprints = merge_event_ids_with_footprint(event_ids, base_path.clone());
     let model = merge_vulnerabilities_with_footprint(footprints, base_path);
 
    let mut buffer = Vec::new();
    
    for i in model {
-       let mut placeholder = HashMap::new();
-       placeholder.insert(String::from("areaperil_id"), Value::Int(i.areaperil_id));
+       let mut placeholder = PyDict::new(py);
+       placeholder.set_item("areaperil_id", i.areaperil_id);
        buffer.push(placeholder);
    }
    return buffer
