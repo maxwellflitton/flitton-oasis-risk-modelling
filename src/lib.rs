e96c47141e1b03b0a6f8a8754a15a2a75ff12ce6
Maxwell Flitton <maxwellflitton@maxwells-mbp.mynet>
2021-08-06 23:16:52 +0100

    basics working

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,24 @@ use vulnerabilities::merge_vulnerabilities_with_footprint;
 use vulnerabilities::structs::VulnerabilityFootPrint;
 
 
+#[derive(Debug)]
+enum Value {
+    Float(f32),
+    Int(i32),
+}
+
+fn main() {
+    let mut map = HashMap::new();
+
+    map.insert("a", Value::Str("1"));
+    map.insert("b", Value::Int(2));
+
+    for (key, value) in &map {
+        println!("{}: {:?}", key, value);
+    }
+}
+
+
 #[pyfunction]
 fn get_model<'a>(event_ids: Vec<i32>, mut base_path: String) -> Vec<HashMap<String, i32>> {
     let footprints = merge_event_ids_with_footprint(event_ids, base_path.clone());
@@ -21,7 +39,7 @@ fn get_model<'a>(event_ids: Vec<i32>, mut base_path: String) -> Vec<HashMap<Stri
    
    for i in model {
        let mut placeholder = HashMap::new();
-       placeholder.insert(String::from("areaperil_id"), i.areaperil_id);
+       placeholder.insert(String::from("areaperil_id"), Value::Int(i.areaperil_id));
        buffer.push(placeholder);
    }
    return buffer
